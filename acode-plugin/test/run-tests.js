c52a8da6efb3eca3ba7335103daa4be803d99b87
Matthieu Schwaiger <matth.schwaig@gmail.com>
2026-09-23 08:10:52 +0200

    test(acode): remove runner self-import cycle

--- acode-plugin/test/run-tests.js
+++ acode-plugin/test/run-tests.js
@@ -1,29 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
-function discoverTestFiles(files) {
-  const testFiles = files.filter(f => f.startsWith('test_') && f.endsWith('.js'));
-  if (testFiles.length === 0) {
-    const error = new Error('no_test_suites_discovered');
-    error.code = 'no_test_suites_discovered';
-    throw error;
-  }
-  return testFiles;
-}
-
-function resolveSuiteRunner(suite, file = '<unknown>') {
-  if (typeof suite === 'function') {
-    return suite;
-  }
-
-  if (suite && typeof suite.run === 'function') {
-    return () => suite.run();
-  }
-
-  const error = new Error(`invalid_test_suite_export:${file}`);
-  error.code = 'invalid_test_suite_export';
-  throw error;
-}
+const { discoverTestFiles, resolveSuiteRunner } = require('./runner-contract');
 
 async function runAllTests() {
   console.log('=== Running Acode Runtime Test Suite ===\n');
@@ -72,4 +49,4 @@ if (require.main === module) {
   });
 }
 
-module.exports = { runAllTests, discoverTestFiles, resolveSuiteRunner };
+module.exports = { runAllTests };
